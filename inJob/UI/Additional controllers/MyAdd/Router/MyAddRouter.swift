058218b87e0a38f063f99306b5f9475976474289
Михаил Мерешков <mereshkov.ms@medsigroup.com>
2022-11-11 17:47:42 +0300

    bagfix/2

---
+++ inJob/UI/Additional controllers/MyAdd/Router/MyAddRouter.swift
@@ -0,0 +1,22 @@
+//
+//  MyAddRouter.swift
+//  inJob
+//
+//  Created by Михаил Мерешков on 10.11.2022.
+//
+
+import UIKit
+
+protocol MyAddRouterProtocol: AnyObject {
+    
+}
+
+final class MyAddRouter: MyAddRouterProtocol {
+
+    // MARK: - Properties
+
+    weak var presenter: UIViewController?
+
+    // MARK: - Functions
+    
+}
